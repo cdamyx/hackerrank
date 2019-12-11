a1883f107293d3571a231f527731313b2265e2cc
Chris Amyx <40224280+cdamyx@users.noreply.github.com>
2019-12-11 15:56:56 -0600

    updated

--- 30DaysCode-10-Binary.js
+++ 30DaysCode-10-Binary.js
@@ -26,35 +26,38 @@ function readLine() {
 
 function main() {
     let n = parseInt(readLine(), 10);
-	let binaryArr = [];
-	let max = 0;
-	let current = 0;
-	
-	//create binary number by taking remainder of n / 2 and pushing to array
-	while (n != 0) {
-		if (n % 2 == 0) {
-			binaryArr.push(0);
-		}
-		else {
-			binaryArr.push(1);
-		}
-		n = Math.floor(n / 2);
-	}
-	
-	//iterate through array and get longest string of 1's
-	for (let i = 0; i <= binaryArr.length; i++) {
-		if (binaryArr[i] == 1) {
-			current++;
-		}
-		else {
-			if (current > max) {
-				max = current;
-				current = 0;
-			}
-		}
-	}
-	
-	console.log(max);
-	
+    let binaryArr = [];
+    let max = 0;
+    let current = 0;
+    
+    //create binary number by taking remainder of n / 2 and pushing to array
+    while (n != 0) {
+        if (n % 2 == 0) {
+            binaryArr.push(0);
+        }
+        else {
+            binaryArr.push(1);
+        }
+        n = Math.floor(n / 2);
+    }
+    
+    //iterate through array and get longest string of 1's
+    for (let i = 0; i <= binaryArr.length; i++) {
+        if (binaryArr[i] == 1) {
+            current++;
+        }
+        else {
+            if (current >= max) {
+                max = current;
+            }
+            current = 0;
+        }
+    }
+    
+    if (current >= max) {
+        max = current;
+    }
+    console.log(max);
+    
 }
 
