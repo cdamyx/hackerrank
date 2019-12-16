82c5f9d487269463e0e723e1f55ef12ac0febf7d
Chris Amyx <40224280+cdamyx@users.noreply.github.com>
2019-12-16 11:12:00 -0600

    completed

---
+++ 30DaysCode-11-2dArray.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    let arr = Array(6);
+    let newArr = [];
+    let count = 0;
+
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+        for (let j = 0; j < 4; j++) {
+            for (let k = 0; k < 4; k++) {
+                newArr[count] = arr[j][k] + arr[j][k+1] + arr[j][k+2] + arr[j+1][k+1] + arr[j+2][k] + arr[j+2][k+1] + arr[j+2][k+2];
+                count++;
+            }
+        }
+
+        newArr.sort(function(a, b){return b-a});
+        console.log(newArr[0]);
+}
