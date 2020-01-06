adecb2a167d8066bb123ee5e149726006382ddc8
Chris Amyx <40224280+cdamyx@users.noreply.github.com>
2020-01-06 10:19:53 -0600

    comments and cleanup

--- 30DaysCode-20-Sorting.js
+++ 30DaysCode-20-Sorting.js
@@ -27,13 +27,14 @@ function main() {
     // Write Your Code Here
     //moved numberOfSwaps outside loop so it isn't reset
     let numberOfSwaps = 0;
+    //handy print function to keep from repeating
     function printer(x, y) {
         console.log("Array is sorted in " + x + " swaps.");
         console.log("First Element: " + y[0]);
         console.log("Last Element: " + y[y.length - 1]);
     };
+    //2D loop
     for (let i = 0; i < a.length; i++) {
-
         for (let j = 0; j < a.length - 1; j++) {
             if (a[j] > a[j + 1]) {
                 let temp = a[j];
@@ -42,11 +43,12 @@ function main() {
                 numberOfSwaps++;
             }
         }
-
+        //break so that outer loop doesn't continue if array is already sorted
         if (numberOfSwaps == 0) {
             printer(numberOfSwaps, a);
             break;
         }
+        //reach the end, print results
         else if (i == a.length - 1) {
             printer(numberOfSwaps, a);
         }
